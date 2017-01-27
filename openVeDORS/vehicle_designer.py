e7bf68f61fbd65f2c3314dbd584b2eaba7fabf76
cflannery <cmflannery@ucsd.edu>
2017-01-26 23:36:40 -0800

    fixing travis ci, initial tests commit

--- openVeDORS/vehicle_designer.py
+++ openVeDORS/vehicle_designer.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python
+from future.utils import raise_from
+
+
+class Rocket:
+    def __init__(self, arg):
+        self.arg = arg
+
+def main():
+    print('Hello python worlds 2 and 3')
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('exiting due to KeyboardInterrupt')
